5f4a3b37760521589f4665116b0f20941e6ef2e4
it21241246 <it21241246@my.sliit.lk>
2023-04-16 21:07:43 +0530

    second commit with teacher backend

---
+++ models/Teacher.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const teacherSchema = new Schema ({
+
+    name : {
+        type : String,
+        required :true
+    },
+
+    nic : {
+        type : Number,
+        required : true
+    },
+
+    address : {
+        type : String,
+        required : true
+    },
+    
+    age : {
+        type : Number,
+        required :true
+    },
+
+    gender : {
+        type : String,
+        required : true
+    },
+
+    land : {
+        type :Number,
+        requred : true
+    },
+
+    mobile : {
+        type : Number,
+        required : true
+    },
+
+    workPlace : {
+        type : String,
+        required : true
+    }
+
+    // image:{
+    //     type : String,
+    //     required :false
+    // }
+})
+
+const Teacher =  mongoose.model("teacher",teacherSchema);
+module.exports = Teacher;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
+const multer = require("multer");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8070;
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 8070;
 app.use(cors());
 app.use(bodyParser.json());
 
+//connect db
 const URL = process.env.MONGODB_URL;
 mongoose.connect(URL,{
    
@@ -24,8 +26,15 @@ connection.once("open", () => {
     console.log("Mongodb Connection Success!");
 });
 
+//data to staff db
 const staffRouter = require("./routes/staff.js");
 app.use("/staff",staffRouter);
+
+
+//db to teacher db
+const teacherRouter = require("./routes/teacher.js");
+app.use("/teacher",teacherRouter);
+
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on port number :  ${PORT}`)
